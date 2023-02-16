c75e7eda12244b261b05515d89cf29739065a19c
Carlos <carlos@falconmasters.com>
2023-02-16 13:58:50 -0600

    Eliminando carpeta de src

--- Proyectos/Peliculas/src/listenerPopup.js
+++
@@ -1,9 +0,0 @@
-const popup = document.getElementById('media');
-
-popup.addEventListener('click', (e) => {
-	e.preventDefault();
-
-	if (e.target.closest('button')) {
-		popup.classList.remove('media--active');
-	}
-});
