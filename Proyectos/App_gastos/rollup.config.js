5288b43462887eebc6c736c9722160e24c00ec1e
Carlos <carlos@falconmasters.com>
2023-02-23 12:51:55 -0600

    Eliminando archivos finales

--- Proyectos/App_gastos/rollup.config.js
+++ Proyectos/App_gastos/rollup.config.js
@@ -1,10 +1,7 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-
 export default {
 	input: 'src/index.js',
 	output: {
 		file: 'public/bundle.js',
 		format: 'cjs',
 	},
-	plugins: [nodeResolve()],
 };
--- Proyectos/App_gastos/src/index.js
+++
@@ -1,8 +0,0 @@
-import './eventosGasto';
-import './eventoBtnFormularioGasto';
-import './agregarGasto';
-import cargarGastos from './cargarGastos';
-import cargarTotalGastado from './cargarTotalGastado';
-
-cargarGastos();
-cargarTotalGastado();
